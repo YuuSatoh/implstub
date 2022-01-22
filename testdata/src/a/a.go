4e847e19ba76f6f15c3e2a7daa5e5aa110f58832
yusato <yex.roh@gmail.com>
2022-01-22 20:03:57 +0900

    remove unnecessary files

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,27 +1,11 @@
 package a
 
-import "fmt"
-
-type DB struct {
+func main() {
+	println("hi") // want "call of println"
+	print("hi")   // not a call of println
 }
 
-type foo struct {
-}
+func println(s string) {} // want println:"found"
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
-}
-
-func (f foo) yey(msg string) error {
-	return nil
-}
-
-func fo(format string, args ...interface{}) {
-	fmt.Printf(format, args...)
-}
-
-func main2() {
-	fo("%d")
+type ADB struct {
 }
